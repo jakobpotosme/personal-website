c03b6b8e33cf71b30f2022af8fa4119d8ebe1c3b
jakobpotosme <56290282+jakobpotosme@users.noreply.github.com>
2022-03-17 21:00:52 -0700

    fixed opensea permalink

--- src/components/Web3/ERC721Tokens/OpenseaTokens.js
+++ src/components/Web3/ERC721Tokens/OpenseaTokens.js
@@ -3,7 +3,7 @@ import React from 'react'
 const OpenseaTokens = ({name,image,key,description,address, floorPrice, link}) => {
   return (
     <div>
-        <div class=" bg-input rounded-lg hover:bg-zinc-500 pt-2 h-auto xl:h-96">
+        <div class=" bg-input rounded-lg hover:bg-zinc-500 pt-2 h-auto md:h-60 xl:h-96">
 
             <div className='token-name' class="container text-xl flex-col text-center ">
               <h1> {name}  - </h1>  
@@ -13,12 +13,13 @@ const OpenseaTokens = ({name,image,key,description,address, floorPrice, link}) =
               
               {/* <p>{address}</p> */}
               {/* <p>{floorPrice}</p> */}
-              <div class="grid">
+              <div class="grid grid-cols-1 gap-y-6">
                 <img class="w-1/3 place-self-center  " src = {image} alt="nft" />
                 {/* <img class=" h-48 place-self-center mb-6 w-full object-cover md:h-full md:w-48" src = {image} alt="nft" /> */}
-                
+                <a href={`${link}`}> <p class="break-words">{link}</p></a>  
               </div>
-              <a  href='${link}'>{link}</a>
+              
+              
               
             </div>
             
